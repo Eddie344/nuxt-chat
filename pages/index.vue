26fd548b419313b0f0d0b50e286bcbd19acb52cd
Bazhenov <eduard.bazhenov34@gmail.com>
2020-02-16 00:47:31 +0300

    v 1.0

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,23 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8>
       <v-card min-width="400">
-        <v-card-title>Nuxt-chat</v-card-title>
+
+        <v-snackbar
+          v-model="snackbar"
+          :timeout="6000"
+          bottom
+        >
+          {{ message }}
+          <v-btn
+            dark
+            text
+            @click="snackbar = false"
+          >
+            Закрыть
+          </v-btn>
+        </v-snackbar>
+
+        <v-card-title>Chat</v-card-title>
         <v-card-text>
           <v-form ref="form" v-model="valid" lazy-validation>
             <v-text-field v-model="name" :counter="16" :rules="nameRules" label="Ваше имя" required></v-text-field>
@@ -31,6 +47,8 @@ export default {
   },
   data: () => ({
     valid: true,
+    snackbar: false,
+    message: '',
     name: "",
     nameRules: [
       v => !!v || "Введите имя",
@@ -40,6 +58,17 @@ export default {
     roomRules: [v => !!v || "Укажите комнату"]
   }),
 
+  mounted() {
+    const {message} = this.$route.query
+    if(message === 'noUser') {
+      this.message = 'Введите данные'
+    }
+    else if(message === 'leftChat') {
+      this.message = 'Вы покинули чат'
+    }
+    this.snackbar = !!this.message
+  },
+
   methods: {
     ...mapMutations(["setUser"]),
     submit() {
@@ -48,8 +77,17 @@ export default {
           name: this.name,
           room: this.room
         };
-        this.setUser(user);
-        this.$router.push("/chat");
+
+        this.$socket.emit('userJoined', user, data => {
+          if(typeof data === 'string') {
+            console.error(data);
+          }
+          else {
+            user.id = data.userId
+            this.setUser(user);
+            this.$router.push("/chat");
+          }
+        })
       }
     }
   }
